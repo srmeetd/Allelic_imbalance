f5cba0a2fbdaaf5ef048e66b6f93af445e70fe2d
Sumeet Deshmukh <45796685+srmeetd@users.noreply.github.com>
2020-02-19 18:24:27 +0000

    Add files via upload

---
+++ allele_imbalance_pipeline.py
@@ -0,0 +1,180 @@
+"""
+rements
+------------
+
+The pipeline requires the results from
+:doc:`pipeline_annotations`. Set the configuration variable
+:py:data:`annotations_database` and :py:data:`annotations_dir`.
+
+On top of the default CGAT setup, the pipeline requires the following
+software to be in the path:
+
+.. Add any additional external requirements such as 3rd party software
+   or R modules below:
+
+Requirements:
+
+* samtools >= 1.1
+
+Pipeline output
+===============
+
+.. Describe output files of the pipeline here
+
+Glossary
+========
+
+.. glossary::
+
+
+Code
+====
+
+"""
+from ruffus import *
+
+import sys
+import os
+import sqlite3
+import shutil
+import CGATCore.Experiment as E
+from CGATCore import Pipeline as P
+import re
+import glob
+import collections
+import CGAT.GTF as GTF
+import CGATCore.IOTools as IOTools
+import CGAT.BamTools.bamtools as BamTools
+import CGATPipelines.PipelineGeneset as PipelineGeneset
+import CGATPipelines.PipelineMapping as PipelineMapping
+import CGATPipelines.PipelineMappingQC as PipelineMappingQC
+
+# Pipeline configuration
+P.get_parameters(
+    ["%s/pipeline.yml" % os.path.splitext(__file__)[0],
+     "../pipeline.yml",
+     "pipeline.yml"],
+    defaults={
+        'paired_end': False})
+
+PARAMS = P.PARAMS
+
+# Add parameters from the annotation pipeline, but
+# only the interface
+#PARAMS.update(P.peek_parameters(
+#    PARAMS["annotations_dir"],
+#    "genesets",
+#    prefix="annotations_",
+#    update_interface=True,
+#    restrict_interface=True))
+
+####merged file#####
+
+@follows(mkdir("merged_vcf"))
+
+def merge_vcf():
+    job_memory = "8G"
+    if not os.path.isfile("merged_vcf/merged_variants.vcf"):
+        statement = '''bcftools merge Variantcalls.dir/*.vcf.gz > merged_vcf/merged_variants.vcf '''
+        P.run(statement)
+    else:
+        pass
+
+####modify genotype####
+
+@follows(merge_vcf,mkdir("correcting_missing_genotype.dir"))
+
+def genotype(): 
+    job_memory="6G"
+    infile="merged_vcf/merged_variants.vcf"
+    outfile="correcting_missing_genotype.dir/merged_variants_corrected.vcf"
+    if not os.path.isfile(outfile):
+        statement= '''bcftools +missing2ref %(infile)s  > %(outfile)s'''
+        P.run(statement)
+    else:
+        pass
+
+####MAF calculation#####
+
+@follows(genotype,mkdir("MAF_score.dir"))
+
+def MAF ():
+    job_memory="6G"
+    infile="correcting_missing_genotype.dir/merged_variants_corrected.vcf"
+    outfile="MAF_score.dir/merged_variants_corrected_MAF.vcf"
+    if not os.path.isfile(outfile):
+        statement = ''' bcftools +fill-tags %(infile)s -Ov -o %(outfile)s -- -t AN,AC,AC_Hemi,AC_Hom,AC_Het,AN,HWE,MAF,NS '''
+        P.run(statement)
+    else:
+        pass
+
+####filter bam files######
+
+@follows(mkdir("sorted_bams.dir"))
+@transform("star.dir/*.bam",regex(".+/(.+).bam"),r"sorted_bams.dir/\1.sorted.bam")
+
+def sort(infiles,outfile):
+    job_memory = "16G"
+    infile=infiles
+    statement = '''samtools sort -n %(infile)s > %(outfile)s'''
+    P.run(statement)
+
+
+@follows(mkdir("Filter_bam.dir"))
+@transform(sort, regex(".+/(.+).genome1.star.sorted.bam"),r"Filter_bam.dir/\1.star.filtered.bam")
+def filter_bam (infiles,outfile):
+    job_memory = "16G"
+    infile=infiles
+    bam1="sorted_bams.dir/" + os.path.basename(infile).split(".",3)[0] + ".genome1.star.sorted.bam"
+    bam2="sorted_bams.dir/" + os.path.basename(infile).split(".",3)[0] + ".genome2.star.sorted.bam"
+   # bam1="sorted_bams.dir/" + os.path.basename(infile).split(".star")[0] + ".star.sorted.bam"
+   # bam2="sorted_bams.dir/" + os.path.basename(infile).split(".star")[0] + ".star.sorted.bam"
+    outfile1 = "Filter_bam.dir/" + os.path.basename(infile).split(".",3)[0] + ".star.filtered.bam"
+
+    outfile="Filter_bam.dir/" + os.path.basename(infile).split(".",3)[0] + ".filtered.bam"
+    statement = '''python /data/md1srd/Software/pipeline_allele_imbalance/pipeline_split_genome/modified_split_genome_pipeline/filter_bam.py %(bam1)s %(bam2)s %(outfile1)s '''
+    P.run(statement)
+
+
+@follows(mkdir("sorted_filter_bam.dir"))
+@transform("Filter_bam.dir/*.filtered.bam",
+           regex(".+/(.+).star.filtered.bam"),
+           r"sorted_filter_bam.dir/\1.sorted.bam")
+def sort_filter (infile, outfile):
+    job_memory = "8G"
+    
+    output = "sorted_filter_bam.dir/" + os.path.basename(infile).split(".")[0] + ".sorted.bam"
+    statement = '''samtools sort %(infile)s > %(output)s && 
+                   samtools index %(output)s '''
+    P.run(statement)
+
+
+@follows(mkdir("Input_Quasar.dir"))
+@transform(sort_filter, 
+           regex(".+/(.+).sorted.bam"),
+           add_inputs(r"Filter_vcf.dir/\1.star.vcf"),
+           r"Input_Quasar.dir/\1.bed")
+           
+
+def input_quasar(infiles,outfile):
+    job_memory = "16G"
+    bam,vcf = infiles
+    fafile="patient_genomes.dir/" + os.path.basename(bam).split(".")[0] + ".star.genome1.fasta"
+    output = "Input_Quasar.dir/" + os.path.basename(bam).split(".")[0] + ".bed"
+    statement = '''python /data/md1srd/Software/pipeline_allele_imbalance/pipeline_split_genome/modified_split_genome_pipeline/base_count.py %(bam)s %(output)s %(fafile)s %(vcf)s'''
+    P.run(statement)
+
+@follows(merge_vcf, genotype, MAF, sort, filter_bam, sort_filter,input_quasar)
+def full():
+    pass
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    P.main(argv)
+
+
+if __name__ == "__main__":
+    sys.exit(P.main(sys.argv))
+
+
---
+++ modified_pipeline_split_genomes.py
@@ -0,0 +1,421 @@
+"""
+rements
+------------
+
+The pipeline requires the results from
+:doc:`pipeline_annotations`. Set the configuration variable
+:py:data:`annotations_database` and :py:data:`annotations_dir`.
+
+On top of the default CGAT setup, the pipeline requires the following
+software to be in the path:
+
+.. Add any additional external requirements such as 3rd party software
+   or R modules below:
+
+Requirements:
+
+* samtools >= 1.1
+
+Pipeline output
+===============
+
+.. Describe output files of the pipeline here
+
+Glossary
+========
+
+.. glossary::
+
+
+Code
+====
+
+"""
+from ruffus import *
+
+import sys
+import os
+import sqlite3
+import shutil
+import CGATCore.Experiment as E
+from CGATCore import Pipeline as P
+import re
+import glob
+import collections
+import CGAT.GTF as GTF
+import CGATCore.IOTools as IOTools
+import CGAT.BamTools.bamtools as BamTools
+import CGATPipelines.PipelineGeneset as PipelineGeneset
+import CGATPipelines.PipelineMapping as PipelineMapping
+import CGATPipelines.PipelineMappingQC as PipelineMappingQC
+
+
+
+# Pipeline configuration
+P.get_parameters(
+    ["%s/pipeline.yml" % os.path.splitext(__file__)[0],
+     "../pipeline.yml",
+     "pipeline.yml"],
+    defaults={
+        'paired_end': False})
+
+PARAMS = P.PARAMS
+
+# Add parameters from the annotation pipeline, but
+# only the interface
+PARAMS.update(P.peek_parameters(
+    PARAMS["annotations_dir"],
+    "genesets",
+    prefix="annotations_",
+    update_interface=True,
+    restrict_interface=True))
+
+PipelineGeneset.PARAMS = PARAMS
+PipelineMappingQC.PARAMS = PARAMS
+
+# Helper functions mapping tracks to conditions, etc
+# determine the location of the input files (reads).
+try:
+    PARAMS["input"]
+except NameError:
+    DATADIR = "."
+else:
+    if PARAMS["input"] == 0:
+        DATADIR = "."
+    elif PARAMS["input"] == 1:
+        DATADIR = "data.dir"
+    else:
+        DATADIR = PARAMS["input"]  # not recommended practise.
+
+def connect():
+    '''connect to database.
+
+    This method also attaches to helper databases.
+    '''
+
+    dbh = sqlite3.connect(PARAMS["database_name"])
+
+    if not os.path.exists(PARAMS["annotations_database"]):
+        raise ValueError(
+            "can't find database '%s'" %
+            PARAMS["annotations_database"])
+
+    statement = '''ATTACH DATABASE '%s' as annotations''' % \
+                (PARAMS["annotations_database"])
+
+    cc = dbh.cursor()
+    cc.execute(statement)
+    cc.close()
+    return dbh
+################################
+#add phasing variant
+
+@follows(mkdir("phased.dir"))
+@transform("Variantcalls.dir/*.vcf.gz", regex(r"Variantcalls.dir/(.+).vcf.gz"),
+            add_inputs(r"split.dir/\1.split.bam"),
+            r"phased.dir/\1.vcf.gz")
+def phasevariants(infiles, outfile):
+    pass
+    vcf, bam = infiles
+    fastamap = PARAMS["mapfasta"]
+    drctry= PARAMS["tmpdir"]
+    tempfile=P.snip(outfile,".gz")
+    statement = ''' java -Xmx10g -Djava.io.tmpdir=%(drctry)s -jar /shared/sudlab1/General/git_repositories/GATK_file/GenomeAnalysisTK-3.8-1-0-gf15c1c3ef/GenomeAnalysisTK.jar
+                          -T ReadBackedPhasing
+                          -R %(fastamap)s
+                          -I %(bam)s
+                          --variant %(vcf)s
+                          -L %(vcf)s
+                          -o %(outfile)s
+                          -fixMisencodedQuals
+                          --phaseQualityThresh 20.0
+                           '''  
+
+    job_memory = "16G"
+    P.run(statement)
+
+@follows(mkdir("Filter_vcf.dir"))
+@transform(phasevariants,regex(r"phased.dir/(.+).vcf.gz"),r"Filter_vcf.dir/\1.vcf")
+def filtering(infile,outfile):
+    statement = ''' bcftools view
+                    --max-alleles 2
+                    --exclude-types indels
+                    %(infile)s
+                    -o%(outfile)s '''
+    job_memory = "16G"
+    P.run(statement)
+
+
+
+
+
+@follows(filtering,mkdir("patient_genomes.dir"))
+@transform(filtering,
+           regex("Filter_vcf.dir/(.+).vcf"),
+           add_inputs(PARAMS["mapfasta"]),
+           r"patient_genomes.dir/\1.fasta")
+def generate_patient_genome(infiles, outfiles):
+    '''This task uses generate_diploid_genome to make a
+    diploid genome specific to each patient'''
+
+    vcf, genome = infiles
+    #sample = os.path.basename(vcf).split(".",2)[0]
+    sample = os.path.basename(vcf).split(".vcf",1)[0]
+
+    #genome2 = os.path.basename(vcf).split(".",2)[0] + ".genome2.fasta" 
+    outfile = P.snip(os.path.basename(vcf), ".vcf")
+
+    if not os.path.isfile("patient_genomes.dir/" + sample + ".genome1.fasta"):
+        path_to_script = os.path.dirname(__file__)
+        statement = ''' python %(path_to_script)s/generate_split_genomes.py
+                          --stdin=%(vcf)s
+                          --genome-fasta=%(genome)s
+                          --log=%(outfile)s.log
+                          --stdout=%(sample)s
+                           '''
+        job_memory = "8G"
+        P.run(statement)
+
+    if not os.path.isfile("patient_genomes.dir/" + sample + ".genome2.fasta"):
+        path_to_script = os.path.dirname(__file__)
+        statement = ''' python %(path_to_script)s/generate_split_genomes.py
+                          --stdin=%(vcf)s
+                          --genome-fasta=%(genome)s
+                          --log=%(outfile)s.log
+                          --stdout=%(sample)s
+                           '''
+        job_memory = "8G"
+        P.run(statement)
+
+
+@follows(generate_patient_genome)
+@transform("patient_genomes.dir/*.fasta",
+           formatter(),
+           "patient_genomes.dir/{basename[0]}.dir")
+def patient_genome_index(infiles, outfile):
+    '''This task uses STAR to generate indeces for
+       every diploid genome specific to each patient'''
+
+    job_threads = 1
+    job_memory = "64G"
+    tempdir = P.get_temp_filename(dir=".")
+    infile = infiles
+    
+
+    if not os.path.exists(outfile):
+#        shutil.rmtree(outdir)
+        statement = ''' mkdir %(outfile)s;
+                     STAR  --runMode genomeGenerate
+                     --genomeDir %(outfile)s
+                     --genomeFastaFiles %(infile)s
+                     --sjdbGTFfile %(gtf)s
+                     --genomeSAsparseD 2
+                     --sjdbOverhang 100 
+                     --outTmpDir %(tempdir)s '''
+        P.run(statement)
+    else:
+        pass
+
+#############################
+
+#########################################################################
+#########################################################################
+#########################################################################
+# Read mapping
+#########################################################################
+
+SEQUENCESUFFIXES = ("*.fastq.1.gz",
+                    "*.fastq.gz",
+                    "*.fa.gz",
+                    "*.sra",
+                    "*.export.txt.gz",
+                    "*.csfasta.gz",
+                    "*.csfasta.F3.gz",
+                    "*.remote",
+                    )
+
+SEQUENCEFILES = tuple([os.path.join(DATADIR, suffix_name)
+                       for suffix_name in SEQUENCESUFFIXES])
+
+SEQUENCEFILES_REGEX = regex(
+    r".*/(\S+).(fastq.1.gz|fastq.gz|fa.gz|sra|csfasta.gz|csfasta.F3.gz|export.txt.gz|remote)")
+
+
+@follows(mkdir("nreads.dir"))
+@transform(SEQUENCEFILES,
+           SEQUENCEFILES_REGEX,
+           r"nreads.dir/\1.nreads")
+def countReads(infile, outfile):
+    '''Count number of reads in input files.'''
+    m = PipelineMapping.Counter()
+    statement = m.build((infile,), outfile)
+    P.run(statement)
+
+#### need to make change so that it should consider each new genome for each fasqt files.
+
+@follows(patient_genome_index, countReads, mkdir("star.dir"))
+@subdivide(SEQUENCEFILES,
+           SEQUENCEFILES_REGEX,
+           [r"star.dir/\1.genome1.star.bam",
+            r"star.dir/\1.genome2.star.bam"])
+def mapReadsWithSTAR(infile,outfiles):
+
+    job_threads = PARAMS["star_threads"]
+    job_memory = PARAMS["star_memory"]
+    
+   # name = os.path.basename(infile).split(os.extsep, 1)[0]
+    name = os.path.basename(infile).split(".fastq")[0]
+
+
+    star_mapping_genome1 =  name + ".star.genome1"
+    star_mapping_genome2 =  name + ".star.genome2"
+
+    for genome in [star_mapping_genome1, star_mapping_genome2]:
+        
+        star_mapping_genome = genome
+        outfile = os.path.join("star.dir", os.path.basename(genome) + ".star.bam")
+        m = PipelineMapping.STAR(
+            executable=P.substitute_parameters(**locals())["star_executable"],
+            strip_sequence=PARAMS["strip_sequence"])
+
+        statement = m.build((infile,), outfile)
+        P.run(statement)
+
+@follows(mapReadsWithSTAR)
+@merge("star.dir/*.bam", "star_stats.tsv")
+def buildSTARStats(infiles, outfile):
+    '''Compile statistics from STAR run
+    Concatenates log files from STAR runs and reformats them as a tab
+    delimited table.
+    Parameters
+    ----------
+    infile: list
+        :term:`bam` files generated with STAR.
+    outfile: str
+        :term: `tsv` file containing statistics about STAR run
+    '''
+
+    data = collections.defaultdict(list)
+    for infile in infiles:
+        fn =  re.sub(r'.star.bam', '', infile) + "Log.final.out"
+        if not os.path.exists(fn):
+            raise ValueError("incomplete run: %s" % infile)
+
+        for line in IOTools.open_file(fn):
+            if "|" not in line:
+                continue
+            header, value = line.split("|")
+            header = re.sub("%", "percent", header)
+            data[header.strip()].append(value.strip())
+
+    keys = list(data.keys())
+    outf = IOTools.open_file(outfile, "w")
+    outf.write("track\t%s\n" % "\t".join(keys))
+    for x, infile in enumerate(infiles):
+        track = P.snip(os.path.basename(infile), ".bam")
+        outf.write("%s\t%s\n" %
+                   (track, "\t".join([data[key][x] for key in keys])))
+    outf.close()
+
+@follows(buildSTARStats)
+@transform(buildSTARStats, suffix(".tsv"), ".load")
+def loadSTARStats(infile, outfile):
+    '''
+    Loads statistics about a star run from a tsv file to a database table -
+    star_stats.
+    Parameters
+    ----------
+    infile: term:`tsv` file containing a table of tophat statistics.
+    outfile: .load file logging database loading
+    '''
+    P.load(infile, outfile)
+
+
+@follows(countReads)
+@merge(countReads, "reads_summary.load")
+def loadReadCounts(infiles, outfile):
+    ''' load the read counts
+    individual read counts are merged and loaded into a table called
+    reads_summary
+    Parameters
+    ----------
+    infile : str
+       Input filename in :term:`tsv` format
+    outfile : str
+       Output filename, the table name is derived from `outfile`
+    '''
+
+    outf = P.get_temp_file(".")
+    outf.write("track\ttotal_reads\n")
+    for infile in infiles:
+        track = P.snip(infile, ".nreads")
+        lines = IOTools.open_file(infile).readlines()
+        nreads = int(lines[0][:-1].split("\t")[1])
+        outf.write("%s\t%i\n" % (track, nreads))
+    outf.close()
+
+    P.load(outf.name, outfile)
+
+    os.unlink(outf.name)
+
+@follows(mapReadsWithSTAR)
+@transform(mapReadsWithSTAR,
+           regex(".bam"),
+           ".bed.gz")
+def buildBed(infile, outfile):
+    ''' Generate :term:`bed` format file from :term:`bam` alignment file
+    Parameters
+    ----------
+    infile : str
+       Input filename in :term:`bam` format
+    outfile : str
+       Output filename in :term:`bed` format
+    '''
+
+    statement = '''
+    cat %(infile)s
+    | cgat bam2bed
+          %(bed_options)s
+          --log=%(outfile)s.log
+          -
+    | sort -k1,1 -k2,2n
+    | bgzip
+    > %(outfile)s;
+    tabix -p bed %(outfile)s
+    '''
+    P.run(statement)
+
+
+@follows(phasevariants,filtering,generate_patient_genome, patient_genome_index,countReads, mapReadsWithSTAR, buildSTARStats, loadSTARStats, loadReadCounts, buildBed)
+def full():
+    pass
+
+
+
+@follows(mkdir("MultiQC_report.dir"))
+@originate("MultiQC_report.dir/multiqc_report.html")
+def renderMultiqc(infile):
+    '''build mulitqc report'''
+
+    statement = (
+        "export LC_ALL=en_GB.UTF-8 && "
+        "export LANG=en_GB.UTF-8 && "
+        "multiqc . -f && "
+        "mv multiqc_report.html MultiQC_report.dir/")
+    P.run(statement)
+
+
+@follows(renderMultiqc)
+def build_report():
+    '''report dummy task to build reports'''
+    pass
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    P.main(argv)
+
+
+if __name__ == "__main__":
+    sys.exit(P.main(sys.argv))
